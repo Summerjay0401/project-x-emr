0b5e3df95366705a128ad16dcaa87133aa80d83b
Ysfboi <jdanleano@gmail.com>
2022-10-24 23:08:20 -0400

    Updated routing with My Profile page with working Go Back button

--- src/project-emr-frontend/src/pages/MyProfile/MyProfile.jsx
+++ src/project-emr-frontend/src/pages/MyProfile/MyProfile.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
 
 export default function MyProfile() {
     return (
       <div class="mx-5 d-flex flex-column">
-        <h1>THIS IS A TEST</h1>
+        <h1>THIS IS THE MY PROFILE TEST</h1>
+        <Link to="/"><Button variant="outlined">Go Back</Button></Link>
       </div>
     );
   }
