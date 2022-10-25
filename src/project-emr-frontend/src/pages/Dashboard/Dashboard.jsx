482ace939ba400529a01b6483277f9916fc2d6a8
Summer Jade <summerjadefrosal@gmail.com>
2022-10-24 21:21:52 -0400

    added dashboard ui

--- src/project-emr-frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/project-emr-frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
-import BasicCard from "../../components/BasicCard";
+import Widget from '../../components/Widget'
 
 const Dashboard = () => {
   return (
       <div>
             <h1>Dashboard</h1>
-            <BasicCard />
-            <BasicCard />
-            <BasicCard />
-            <BasicCard />
+            <div className="widgets">
+                <Widget type="my-profile" />
+                <Widget type="appointments" />
+                <Widget type="progress-notes" />
+                <Widget type="laboratory-results" />
+                <Widget type="my-medications" />
+                <Widget type="messages" />
+                <Widget type="telehealth" />
+                <Widget type="payments" />
+            </div>
     </div>
   )
 }
