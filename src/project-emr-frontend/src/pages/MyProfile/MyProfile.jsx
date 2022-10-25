1cb3eca221cbbe90fd0b3cfd914d14213e9a345a
Ysfboi <jdanleano@gmail.com>
2022-10-24 22:45:59 -0400

    Added route for My Profile Page. Tested for proper functionality

---
+++ src/project-emr-frontend/src/pages/MyProfile/MyProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MyProfile() {
+    return (
+      <div class="mx-5 d-flex flex-column">
+        <h1>THIS IS A TEST</h1>
+      </div>
+    );
+  }
