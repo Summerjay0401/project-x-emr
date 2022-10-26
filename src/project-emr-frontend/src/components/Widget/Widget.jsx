21bbc0ed57ee3df2c45e78cc1f6f9e1169c7dbcb
Summer Jade <summerjadefrosal@gmail.com>
2022-10-26 13:39:48 -0400

    added navigate to dashboard

--- src/project-emr-frontend/src/components/Widget/Widget.jsx
+++ src/project-emr-frontend/src/components/Widget/Widget.jsx
@@ -1,16 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserDoctor, faUser, faCalendarDays, faClipboard, faFlask, faPills, faEnvelope, faCreditCard } from '@fortawesome/free-solid-svg-icons' // <-- import styles to be used
 
+import { useNavigate } from "react-router-dom";
 
 import "./Widget.css";
 
 const Widget = ({ type }) => {
+  const navigate = useNavigate();
   let data;
+
+  const handleLinks = (link) => {
+    navigate(link);
+  }
 
   switch (type) {
     case "my-profile":
       data = {
         title: "MY PROFILE",
+        link: "/my-profile",
         icon: (
             <FontAwesomeIcon icon={faUser}
             className="icon"
@@ -24,6 +31,7 @@ const Widget = ({ type }) => {
     case "appointments":
       data = {
         title: "APPOINTMENTS",
+        link: "/appointments",
         icon: (
             <FontAwesomeIcon icon={faCalendarDays}
             className="icon"
@@ -37,6 +45,7 @@ const Widget = ({ type }) => {
     case "progress-notes":
       data = {
         title: "PROGRESS NOTES",
+        link: "/progress-notes",
         icon: (
             <FontAwesomeIcon icon={faClipboard} 
             className="icon"
@@ -50,6 +59,7 @@ const Widget = ({ type }) => {
     case "laboratory-results":
       data = {
         title: "LAB RESULTS",
+        link: "lab-results",
         icon: (
             <FontAwesomeIcon icon={faFlask}
             className="icon"
@@ -62,7 +72,8 @@ const Widget = ({ type }) => {
         break;
     case "my-medications":
     data = {
-        title: "MY MEDICATIONS",
+      title: "MY MEDICATIONS",
+      link: "my-medications",
         icon: (
             <FontAwesomeIcon icon={faPills}             
                 className="icon"
@@ -75,7 +86,8 @@ const Widget = ({ type }) => {
     break;  
     case "messages":
     data = {
-        title: "MY MESSAGES",
+      title: "MY MESSAGES",
+      link: "messages",
         icon: (
             <FontAwesomeIcon icon={faEnvelope}
             className="icon"
@@ -88,7 +100,8 @@ const Widget = ({ type }) => {
     break;  
     case "telehealth":
     data = {
-        title: "CHAT WITH MY PROVIDER",
+      title: "CHAT WITH MY PROVIDER",
+      link: "telehealth",
         icon: (
             <FontAwesomeIcon icon={faUserDoctor}
             className="icon"
@@ -101,7 +114,8 @@ const Widget = ({ type }) => {
     break;  
     case "payments":
     data = {
-        title: "PAYMENTS",
+      title: "PAYMENTS",
+      link: "payments",
         icon: (
             <FontAwesomeIcon icon={faCreditCard}
             className="icon"
@@ -117,11 +131,10 @@ const Widget = ({ type }) => {
   }
 
   return (
-      <div className="widget">
-          <div className="left">
-          {data.icon}
+    <div className="widget" onClick={() => handleLinks(data.link)}>
+      <div className="left">
+        {data.icon}
         <span className="title">{data.title}</span>
-        
       </div>
     </div>
   );
