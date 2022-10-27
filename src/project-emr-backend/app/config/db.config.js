336aa8d5a2240b406c812a74776c9c394da7b7f3
Ysfboi <jdanleano@gmail.com>
2022-10-27 13:12:49 -0400

    Editing profile still

--- src/project-emr-backend/app/config/db.config.js
+++ src/project-emr-backend/app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "",
+  PASSWORD: "rosejenn",
   DB: "emr_db",
   dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
--- src/project-emr-frontend/src/pages/MyProfile/MyProfile.jsx
+++ src/project-emr-frontend/src/pages/MyProfile/MyProfile.jsx
@@ -2,90 +2,167 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import PageTitle from '../../components/PageTitle';
+// import PageTitle from '../../components/PageTitle';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 
-
 export default function myProfile() {
     return (
       <div>
+        <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm">
         <Box 
           sx={{
             width: 200,
             height: 200,
             border: 'solid',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
             '&:hover': {
               backgroundColor: 'primary.main',
               opacity: [0.9, 0.8, 0.7],
             },
             }}>
-              <h1>PICTURE GOES HERE</h1>
+              <p>PICTURE GOES HERE</p>
+        </Box>
+
+        <Box>
+          <List>
+            <ListItem>
+              <ListItemText> Patient ID </ListItemText>
+              <ListItemText> ID here </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> First Name </ListItemText>
+              <ListItemText> First Name Here </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Last Name </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Gender </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Date of Birth </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Address </ListItemText>
+                <ListItemText>
+                  <ListItemText>Line 1</ListItemText>
+                  <ListItemText>Line 2</ListItemText>
+                </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Primary Phone </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Alternative Phone </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Emergency Contact </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+          </List>
+
+          <Divider />
+
+          <List>
+            <ListItem>
+              <ListItemText> Height </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Weight </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Blood Pressure </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Pulse </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+          </List>
+
+          <Divider />
+          
+          <List>
+            <ListItem>
+              <ListItemText> PCP </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Diagnosis </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> ICD10 Code </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Date of Diagnosis </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+          </List>
+
+          <Divider />
+          
+          <List>
+            <ListItem>
+              <ListItemText> Insurance Name </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Member ID </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Group ID (if applicable) </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+            <ListItem>
+              <ListItemText> Provider Services # </ListItemText>
+              <ListItemText> 123123 </ListItemText>
+            </ListItem>
+
+          </List>
           </Box>
-
-          <Box sx={{width: '300px', maxWidth: '300px', bgcolor: 'background.paper' }}>
-        <List>
-        <ListItem>
-            <ListItemText sx={{ width: '150px'}}> Patient ID </ListItemText>
-            <ListItemText> 123123 </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '150px'}}> First Name </ListItemText>
-            <ListItemText> 123123 </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '150px'}}> Last Name </ListItemText>
-            <ListItemText> 123123 </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '150px'}}> Gender </ListItemText>
-            <ListItemText> 123123 </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '150px'}}> Date of Birth </ListItemText>
-            <ListItemText> 123123 </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '150px'}}> Address </ListItemText>
-            <ListItemText>
-              <ListItemText>Line 1</ListItemText>
-              <ListItemText>Line 2</ListItemText>
-            </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '150px'}}> Primary Phone </ListItemText>
-            <ListItemText> 123123 </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '150px'}}> Alternative Phone </ListItemText>
-            <ListItemText> 123123 </ListItemText>
-          </ListItem>
-
-          <ListItem>
-            <ListItemText sx={{ width: '500px'}}> Emergency Contact 
-              <ListItemText> Name </ListItemText>
-              <ListItemText> Number </ListItemText>
-            </ListItemText>
-            <ListItemText sx={{ width: '500px'}}> Emergency Contact 
-              <ListItemText> Name </ListItemText>
-              <ListItemText> Number </ListItemText>
-            </ListItemText>
-          </ListItem>
-
-
-        </List>
-        <Divider />
-      </Box>
+          </Container>
+        </React.Fragment>
 
         <Link to="/"><Button variant="outlined">Go Back</Button></Link>
       </div>
