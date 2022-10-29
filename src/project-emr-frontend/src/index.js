3246b9946599ce5cb2493796b8dcbceda9a261b2
Summer Jade <summerjadefrosal@gmail.com>
2022-10-29 12:44:06 -0400

    fix react-dom

--- src/project-emr-frontend/src/index.js
+++ src/project-emr-frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
