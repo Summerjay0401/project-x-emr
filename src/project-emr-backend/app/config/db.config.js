8afac64222a48e1bddb2995c045e90f4979e6163
Ajalloh1 <mys4jalloh@gmail.com>
2022-11-01 18:59:18 -0400

    added routing

--- src/project-emr-backend/app/config/db.config.js
+++ src/project-emr-backend/app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "",
+  PASSWORD: "Conakry1",
   DB: "emr_db",
   dialect: "mysql",
   pool: {
--- src/project-emr-frontend/src/pages/Telehealth/Telehealth.jsx
+++ src/project-emr-frontend/src/pages/Telehealth/Telehealth.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 import PageTitle from '../../components/PageTitle';
 
+import DoctorsList from '../DoctorsList/DoctorsList';
+
 export default function telehealth() {
     return (
       <div>
         <PageTitle title="CHAT WITH YOUR PROVIDER"/>
+        <DoctorsList />
         <Link to="/"><Button variant="outlined">Go Back</Button></Link>
       </div>
     );
