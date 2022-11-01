2bc4e5be87d6540385b1a7cd8a1f745c33f983d1
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-31 23:14:08 -0400

    added doctors specialties using MUI

--- src/project-emr-frontend/src/pages/DoctorsList/DoctorsList.jsx
+++ src/project-emr-frontend/src/pages/DoctorsList/DoctorsList.jsx
@@ -15,4 +15,105 @@ const DoctorsList = () => {
   )
 }
 
-export default DoctorsList
+// export default DoctorsList
+
+// import * as React from 'react';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemText from '@mui/material/ListItemText';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
+// import Avatar from '@mui/material/Avatar';
+// import ImageIcon from '@mui/icons-material/Image';
+// import WorkIcon from '@mui/icons-material/Work';
+// import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+
+export default function DoctorsList() {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Therapists" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Surgeon" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Dermatologist"/>
+      </ListItem>
+    </List>
+  );
+}
+export default function DoctorsList() {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Cardiologist" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Pediatrician" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Dentist"/>
+      </ListItem>
+    </List>
+  );
+}
+export default function DoctorsList() {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Allergist" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Endocrinologist" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Opthalmologist"/>
+      </ListItem>
+    </List>
+  );
+}
