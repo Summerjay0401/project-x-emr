7b635e375d0bc4226e958ad7cf664ab0b6ef9887
Ajalloh1 <mys4jalloh@gmail.com>
2022-11-02 00:54:26 -0400

    added Doctors list cards

--- src/project-emr-frontend/src/pages/DoctorsList/DoctorsList.jsx
+++ src/project-emr-frontend/src/pages/DoctorsList/DoctorsList.jsx
@@ -2,17 +2,41 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 
-import PageTitle from '../../components/PageTitle'
+import PageTitle from '../../components/PageTitle';
 
-const DoctorsList = () => {
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function DoctorsList() {
   return (
-    <div>
-      <PageTitle title="Medical Center 👨🏻‍⚕️ Find your Doctor Here" />
-      <Link to="/">
-        <Button variant="outlined">Go Back</Button>
-      </Link>
-    </div>
-  )
+  <Card sx={{ maxWidth: 180 }}>
+    <CardMedia
+      component="img"
+      alt="green iguana"
+      height="140"
+      image="https://source.unsplash.com/random"
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        Dr. Dan Ross
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+       BDS MDS - Periodontology 
+        and Oral implantology. 20 Years 
+        experience.
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Share</Button>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </Card>
+);
 }
+// export default DoctorsList
 
-export default DoctorsList
+// import * as React from 'react';
