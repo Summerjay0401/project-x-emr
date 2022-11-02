d0ce9f0823184fe33ee7017faf85efd61536fec2
Jovy Ira <ira_bonching@yahoo.com>
2022-11-01 21:09:15 -0400

    fixed the name and email

--- src/project-emr-frontend/src/components/AuthDropdown/fake-db.js
+++ src/project-emr-frontend/src/components/AuthDropdown/fake-db.js
@@ -1,7 +1,7 @@
 export const authUser = {
-    email: "harmaynicroft@example.com",
-    name: "Harmayni Croft",
+    email: "RossKing@yahoo.com",
+    name: "Ross King",
     profile_pic: "https://ca.slack-edge.com/T03CXGUSFDZ-U03ENPQEHB2-1f3a22c4b411-512",
-    handle: "harmaynicroft@example.com",
+    handle: "RossKing@yahoo.com",
     job_title: "Creative Head"
 };
