3a2435d275011e906a85e712e095fccd64affa36
Summer Jade <summerjadefrosal@gmail.com>
2022-11-02 10:43:09 -0400

    telehealth debugged

--- src/project-emr-frontend/src/pages/Telehealth/Telehealth.jsx
+++ src/project-emr-frontend/src/pages/Telehealth/Telehealth.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 import PageTitle from '../../components/PageTitle';
+import DoctorsList from '../DoctorsList';
 
 export default function telehealth() {
     return (
       <div>
         <PageTitle title="CHAT WITH YOUR PROVIDER"/>
         <Link to="/"><Button variant="outlined">Go Back</Button></Link>
+
+        <DoctorsList />
+
       </div>
     );
   }
