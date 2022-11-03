c3c125f3303b1d7567e36b3c7d09ed98e64f797d
Jovy Ira <ira_bonching@yahoo.com>
2022-11-03 18:14:50 -0400

    added notes to notification

--- src/project-emr-frontend/src/pages/Notifications/Notifications.jsx
+++ src/project-emr-frontend/src/pages/Notifications/Notifications.jsx
@@ -20,15 +20,12 @@ export default function ControlledAccordions() {
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            Dr.Summer Jade Rimorin
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Hi Summer, here are some instructions to make sure your upcoming appointment is a success: </Typography>
+          {/* <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography> */}
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
+          Please be sure to fast for the 12 hours leading up to your appointment (water is ok) and arrive at least 10 minutes before your appointment time to fill out the necessary paperwork.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -39,16 +36,14 @@ export default function ControlledAccordions() {
           aria-controls="panel2bh-content"
           id="panel2bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>Dr. Dan Ross</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
-            You are currently not an owner
-          </Typography>
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Confirm an appointment</Typography>
+          {/* <Typography sx={{ color: 'text.secondary' }}>
+            ""
+          </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
-            varius pulvinar diam eros in elit. Pellentesque convallis laoreet
-            laoreet.
+          Confirmation: You scheduled an appointment with Dr. Ross Ross for July 15,2023 and 1pm. Reply with “Reschedule” to change the date or time or “Cancel” to cancel your appointment.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -60,11 +55,11 @@ export default function ControlledAccordions() {
           id="panel3bh-header"
         >
           <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            Dr. Jovy Ira Naraga
+           How are you doing since your last procedure?
           </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
+          {/* <Typography sx={{ color: 'text.secondary' }}>
             Filtering has been entirely disabled for whole web server
-          </Typography>
+          </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -80,10 +75,8 @@ export default function ControlledAccordions() {
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            Dr.Summer Jade Rimorin
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Thank you for visiting us today. Please rate your experience and share your feedback with us.</Typography>
+          {/* <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography> */}
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -99,10 +92,10 @@ export default function ControlledAccordions() {
           aria-controls="panel2bh-content"
           id="panel2bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>Dr. Dan Ross</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>The lorem ipsum gets its name from the Latin phrase Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.</Typography>
+          {/* <Typography sx={{ color: 'text.secondary' }}>
             You are currently not an owner
-          </Typography>
+          </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -119,9 +112,26 @@ export default function ControlledAccordions() {
           aria-controls="panel3bh-content"
           id="panel3bh-header"
         >
-          <Typography sx={{ width: '33%', flexShrink: 0 }}>
-            Dr. Jovy Ira Naraga
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>The lorem ipsum gets its name from the Latin phrase Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.</Typography>
+          {/* <Typography sx={{ color: 'text.secondary' }}>
+            Filtering has been entirely disabled for whole web server
+          </Typography> */}
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
           </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>The lorem ipsum gets its name from the Latin phrase Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.</Typography>
           <Typography sx={{ color: 'text.secondary' }}>
             Filtering has been entirely disabled for whole web server
           </Typography>
