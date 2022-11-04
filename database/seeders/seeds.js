2ce154604145aabdfe0da3b1ab0ddddaa77aa8d9
Your Name <you@example.com>
2022-11-03 21:19:37 -0400

    seederssers

---
+++ database/seeders/seeds.js
@@ -0,0 +1,12 @@
+// const db = require('../config/connection');
+// const { User } = require('../models');
+
+// const PatientData = require('./patientDataSeeds.json');
+
+// db.once('open', async () => {
+//     await User.deleteMany({});
+
+// }
+
+// ??
+
