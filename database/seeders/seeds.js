d1070ed71f37b9afabff4fa8b0cec3389f3bd752
Your Name <you@example.com>
2022-11-05 13:10:16 -0400

    seeds.js

--- database/seeders/seeds.js
+++ database/seeders/seeds.js
@@ -1,12 +1,23 @@
-// const db = require('../config/connection');
-// const { User } = require('../models');
+const db = require('../config/connection');
+const { User } = require('../models');
 
-// const PatientData = require('./patientDataSeeds.json');
+const patientData = require('./patientDataSeeds.json');
 
-// db.once('open', async () => {
-//     await User.deleteMany({});
+db.once('open', async () => {
+    await patientData.deleteMany({});
+    try {
+        await User.collection.insertMany(patientData);
+        console.log('User Added!');
+        //close db connection
+        process.exit(0);
 
-// }
+    //catch error
+    } catch(e) {
+        console.log("Something went wrong.");
+        console.log(e);
+        process.exit();
+    }
+});
 
-// ??
+
 
