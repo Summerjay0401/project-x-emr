c4a062d47ed9adf7b4f7d1259b751782b091589f
Summer Jade <summerjadefrosal@gmail.com>
2022-11-12 01:09:48 -0600

    troubleshoot 1

--- src/project-emr-backend/server.js
+++ src/project-emr-backend/server.js
@@ -34,22 +34,23 @@ db.sequelize.sync();
 //   res.json({ message: "Welcome to application." });
 // });
 
-  // When our project is pushed to production, we will serve // the react app using express.static() middleware
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "../project-emr-frontend/build")));
-
-    // Any request not caught by our API will be routed
-    // to our built react app
-    app.get("*", function (req, res) {
-      res.sendFile(path.resolve(__dirname, "../project-emr-frontend/build", "index.html"));
-    });
-  }
 // routes
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 require('./app/routes/payment.routes')(app);
 require('./app/routes/patient.routes')(app);
 require('./app/routes/vitals.routes')(app);
+
+// When our project is pushed to production, we will serve // the react app using express.static() middleware
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../project-emr-frontend/build")));
+
+  // Any request not caught by our API will be routed
+  // to our built react app
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../project-emr-frontend/build", "index.html"));
+  });
+}
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
